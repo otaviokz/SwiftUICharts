4c7dccc3955a866b1c9c9e819baef814f2fe3520
Otavio Zabaleta <otaviokz@gmail.com>
2022-10-16 07:14:18 -0300

    wip

--- SwiftUIChartdsDemoApp/SwiftUIChartdsDemoAppTests/SwiftUIChartdsDemoAppTests.swift
+++ SwiftUIChartdsDemoApp/SwiftUIChartdsDemoAppTests/SwiftUIChartdsDemoAppTests.swift
@@ -5,19 +5,19 @@
 //  Created by Otávio Zabaleta on 24/09/2022.
 //
 
-import XCTest
-@testable import SwiftUIChartdsDemoApp
-
-class SwiftUIChartdsDemoAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+//import XCTest
+//@testable import SwiftUIChartdsDemoApp
+//
+//class SwiftUIChartdsDemoAppTests: XCTestCase {
+//
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
 //    func testExample() throws {
 //        // This is an example of a functional test case.
 //        // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,12 +25,11 @@ class SwiftUIChartdsDemoAppTests: XCTestCase {
 //        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
 //        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
 //    }
-
+//
 //    func testPerformanceExample() throws {
 //        // This is an example of a performance test case.
 //        self.measure {
 //            // Put the code you want to measure the time of here.
 //        }
 //    }
-
-}
+//}
